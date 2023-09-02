0533d6f64d454d0f312c8b4e8b5169170ce55b09
ocph23 <ocph23@gmail.com>
2023-09-02 18:44:30 +0900

    publish 2

--- client/apps/js/services/helper.services.js
+++ client/apps/js/services/helper.services.js
@@ -2,8 +2,6 @@ angular.module("helper.service", [])
     .factory("helperServices", helperServices)
     ;
 
-
-
 function helperServices() {
     var service = {};
    // service.url = "http://localhost:3000";
